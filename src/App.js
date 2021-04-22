91f483cc4703d6748579747c5cd8ff37e20a65a0
Priyam Khatri <priyamkhatri24@gmail.com>
2021-04-22 16:49:27 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./App.module.css";
+
+const App = () => {
+  return (
+    <div className={classes.app}>
+      <h1>Let's get started Prime!</h1>
+    </div>
+  );
+};
+
+export default App;
